38e0c369ced4f304638063b3fc066238fe247263
wawhal <rishichandra.wawhal@gmail.com>
2017-11-22 18:27:47 +0530

    Test

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -26,13 +26,51 @@ def test():
 @app.route('/confirm', methods=['POST'])
 def confirm():
     data = request.form
+    print ("================")
     print (data)
+    print ("================")
     return ok
 
 def sendChoice():
     url = "https://hooks.slack.com/services/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"
     payload = {
-        "text": "This is a line of text.\nAnd this is another one."
+        "text": "Would you like to play a game?",
+        "attachments": [
+            {
+                "text": "Choose a game to play",
+                "fallback": "You are unable to choose a game",
+                "callback_id": "wopr_game",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "game",
+                        "text": "Chess",
+                        "type": "button",
+                        "value": "chess"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Falken's Maze",
+                        "type": "button",
+                        "value": "maze"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Thermonuclear War",
+                        "style": "danger",
+                        "type": "button",
+                        "value": "war",
+                        "confirm": {
+                            "title": "Are you sure?",
+                            "text": "Wouldn't you prefer a good game of chess?",
+                            "ok_text": "Yes",
+                            "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }
+        ]
     }
     headers = {
         'content-type': "application/json",
