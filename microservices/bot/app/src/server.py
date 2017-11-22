58333746509f08e6375548f0d7dc9da16ec25268
wawhal <rishichandra.wawhal@gmail.com>
2017-11-22 17:55:30 +0530

    test

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -17,8 +17,33 @@ def event():
     except Exception as e:
         print(e)
         raise
-    return("OK")
+    return (sendChoice())
 
 @app.route('/test', methods=['GET'])
 def test():
     return "Running"
+
+def sendChoice():
+    choiceResponse = {
+        "text": "Are you freaking serious you want to send this message?",
+        "attachments": [
+            {
+                "text": "Please decide",
+                "fallback": "You are unable to choose a game",
+                "callback_id": "wopr_game",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "confirm": {
+                            "title": "Are you sure?",
+                            "text": "Are you really sure?",
+                            "ok_text": "Yes",
+                            "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+    return choiceResponse
