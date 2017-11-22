4cd5f363a934c382856b94389d543af7519569bc
wawhal <rishichandra.wawhal@gmail.com>
2017-11-22 19:42:51 +0530

    DeploymentPush

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -4,21 +4,24 @@ import requests
 import json
 import pprint
 
+token = os.environ['SLACK_TOKEN']
+
 @app.route('/', methods=['POST'])
 def event():
     try:
         data = request.form.to_dict()
-        print ("===============================================================");
         print (data)
-        print ("===============================================================")
         receivedToken = data["token"]
-        receivedTeamId = data["team_id"]
-        sendChoice()
-
+        if (receivedToken == token):
+            receivedText= data["text"]
+            id = storeText(receivedText, data["response_url"])
+            sendChoice(id)
+            return "Waiting for response"
     except Exception as e:
         print(e)
         raise
-    return "OK"
+    
+    return "ok"
 
 @app.route('/test', methods=['GET'])
 def test():
@@ -27,13 +30,14 @@ def test():
 @app.route('/confirm', methods=['POST'])
 def confirm():
     data = request.form.to_dict()
-    print ("================")
     print (data)
-    print ("================")
+    receivedToken = data["payload"]["token"]
+    if (receivedToken == token):
+        return "ok"
     return "ok"
 
-def sendChoice():
-    url = "https://hooks.slack.com/services/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"
+
+def sendChoice(id):
     payload = {
         "text": "Are you sure you want to send a message?",
         "attachments": [
@@ -46,15 +50,17 @@ def sendChoice():
                 "actions": [
                     {
                         "name": "choice",
-                        "text": "Nope",
+                        "text": "Yep",
                         "type": "button",
-                        "value": "no"
+                        "value": "yes",
+                        "id": id
                     },
                     {
                         "name": "choice",
-                        "text": "Yep",
+                        "text": "Nope",
                         "type": "button",
-                        "value": "yes"
+                        "value": "no",
+                        "id": id
                     }
                 ]
             }
@@ -64,5 +70,35 @@ def sendChoice():
         'content-type': "application/json",
     }
 
-    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+    response = requests.request("POST", responseUrl, data=json.dumps(payload), headers=headers)
     print(response.text)
+
+def storeText(text, responseUrl):
+    url = "https://data.hasura/v1/query"
+
+    requestPayload = {
+        "type": "insert",
+        "args": {
+            "table": "slack_messages",
+            "objects": [
+                {
+                    "message": text,
+                    "response_url": response_url
+                }
+            ],
+            "returning": [
+                "id"
+            ]
+        }
+    }
+
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+    print (resp.content)
+    id = resp.content["returning"][0]["id"]
+    return id
