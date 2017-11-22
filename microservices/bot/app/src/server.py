0cca3ed50d42c7fee749dfeb12f776b2000aa130
wawhal <rishichandra.wawhal@gmail.com>
2017-11-22 18:16:35 +0530

    Added a callback endpoint

--- microservices/bot/app/src/server.py
+++ microservices/bot/app/src/server.py
@@ -31,10 +31,10 @@ def confirm():
 
 def sendChoice():
     url = "https://hooks.slack.com/services/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"
-    choiceResponse = "{\n        \"text\": \"Are you freaking serious you want to send this message?\",\n        \"attachments\": [\n            {\n                \"text\": \"Please decide\",\n                \"fallback\": \"You are unable to choose a game\",\n                \"callback_id\": \"wopr_game\",\n                \"color\": \"#3AA3E3\",\n                \"attachment_type\": \"default\",\n                \"actions\": [\n                    {\n                        \"confirm\": {\n                            \"title\": \"Are you sure?\",\n                            \"text\": \"Are you really sure?\",\n                            \"ok_text\": \"Yes\",\n                            \"dismiss_text\": \"No\"\n                        }\n                    }\n                ]\n            }\n        ]\n    }"
+    payload = {"text":"yo sup?"}
     headers = {
         'content-type': "application/json",
     }
 
-    response = requests.request("POST", url, data=choiceResponse, headers=headers)
+    response = requests.request("POST", url, data=payload, headers=headers)
     print(response.text)
